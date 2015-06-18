3c68c78627a4a6bb35e395c4c1a3705843bb76d8
jquense <monastic.panic@gmail.com>
2015-06-18 18:02:30 -0400

    Portal tests

--- src/Portal.js
+++ src/Portal.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { OverlayMixin } from './OverlayMixin';
 
-let prop = 'hi';
-
-let obj = { prop: ...prop };
-
 let Portal = React.createClass({
-
-  prop: obj,
 
   displayName: 'Portal',
 
   // we use the mixin for now, to avoid duplicating a bunch of code.
-  // when the deprecation is removed we need to move the logic here from OVerlayMixin
+  // when the deprecation is removed we need to move the logic here from OverlayMixin
   mixins: [ OverlayMixin ],
 
   propTypes: {
